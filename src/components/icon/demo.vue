4bfd2f07e46f4b3fbec9b3d2a19b115782fdfb00
LiangJiaYu <1013834609@qq.com>
2020-06-19 23:30:23 +0800

    feature: icon-demo补充

--- src/components/icon/demo.vue
+++ src/components/icon/demo.vue
@@ -1,5 +1,55 @@
 <template>
   <div>
-    <ko-icon name="app-indicator"></ko-icon>
+    <div class="main-wrap">
+      <div class="box" v-for="item in nameArray" :key="item">
+        <div class="box-bd">
+          <ko-icon :name="item"></ko-icon>
+        </div>
+        <div class="box-name">{{ item }}</div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+const nameArray = require('@/utils/icon.json');
+
+export default Vue.extend({
+  data() {
+    return {
+      nameArray: nameArray,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.main-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  .box {
+    width: 120px;
+    margin: 0 15px 20px;
+    margin-bottom: 20px;
+    color: #333;
+    &:hover {
+      color: #409eff;
+    }
+  }
+  .box-bd {
+    padding: 30px 0;
+    border-radius: 6px;
+    background-color: #f8f9fa;
+    text-align: center;
+    font-size: 30px;
+    line-height: 0;
+  }
+  .box-name {
+    margin-top: 15px;
+    text-align: center;
+    font-size: 14px;
+  }
+}
+</style>
--- src/components/iconx/demo.vue
+++ src/components/iconx/demo.vue
@@ -1,5 +1,55 @@
 <template>
   <div>
-    <ko-iconx name="app-indicator" size="100px" color="red"></ko-iconx>
+    <div class="main-wrap">
+      <div class="box" v-for="item in nameArray" :key="item">
+        <div class="box-bd">
+          <ko-iconx :name="item"></ko-iconx>
+        </div>
+        <div class="box-name">{{ item }}</div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+const nameArray = require('@/utils/icon.json');
+
+export default Vue.extend({
+  data() {
+    return {
+      nameArray: nameArray,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.main-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  .box {
+    width: 120px;
+    margin: 0 15px 20px;
+    margin-bottom: 20px;
+    color: #333;
+    &:hover {
+      color: #409eff;
+    }
+  }
+  .box-bd {
+    padding: 30px 0;
+    border-radius: 6px;
+    background-color: #f8f9fa;
+    text-align: center;
+    font-size: 30px;
+    line-height: 0;
+  }
+  .box-name {
+    margin-top: 15px;
+    text-align: center;
+    font-size: 14px;
+  }
+}
+</style>
